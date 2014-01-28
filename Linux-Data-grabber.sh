d4011d644be4e15a9d4c7e8ceb6b3dcd1de97707
braneed <reedum@gmail.com>
2014-01-28 10:10:20 -0500

    Added lsof for networks for Merman

--- Linux-Data-grabber.sh
+++ Linux-Data-grabber.sh
@@ -159,6 +159,9 @@ for table in filter nat mangle raw; do
 done
 arp -a >> $TEMP/state/arp_table.txt
 ifconfig -a >> $TEMP/state/interface_configs.txt
+#   Added listening services in list sorted by port and full info about the service
+lsof -i -nlP | awk '{print $1, $8, $9}' | sort -u >> services_unsorted.txt
+lsof -i -nlP | awk '{print $9, $8, $1}' | sed 's/.*://' | sort -u >> $TEMP/state/services_sorted.txt
 }
 
 #**********************************************
